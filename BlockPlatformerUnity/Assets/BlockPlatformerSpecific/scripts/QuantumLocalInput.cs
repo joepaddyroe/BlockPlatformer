5c7c829feed7561534387503c5dee955839f72fa
Joe Roe <joe.roe@unit9.com>
2024-06-22 19:33:42 +0100

    New Unity Input System
    - added WASD and gamepad Left axis movement
    - added Jump with space bar and gamepad A/South Face button

--- BlockPlatformerUnity/Assets/BlockPlatformerSpecific/scripts/QuantumLocalInput.cs
+++ BlockPlatformerUnity/Assets/BlockPlatformerSpecific/scripts/QuantumLocalInput.cs
@@ -1,13 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Deterministic;
 using Quantum;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class QuantumLocalInput : MonoBehaviour
 {
+
+    [SerializeField] private InputActionAsset _inputActions;
+
+    private InputAction _moveInputAction;
+    private InputAction _jumpInputAction;
+    
     void Start()
     {
+        // grab and assign all of the input actions from the input action asset
+        _moveInputAction = _inputActions.FindAction("Move");
+        _jumpInputAction = _inputActions.FindAction("Jump");
+        
+        // this will need to be handled in some kind of UI/Gameplay state switch
+        _inputActions.FindActionMap("Gameplay").Enable();
+        
+        // Subscribe to the input poll callback
         QuantumCallback.Subscribe(this, (CallbackPollInput callback) => PollInput(callback));
     }
 
@@ -16,13 +32,11 @@ public class QuantumLocalInput : MonoBehaviour
         Quantum.Input input = new Quantum.Input();
 
         // Note: Use GetButton not GetButtonDown/Up Quantum calculates up/down itself.
-        input.Jump = UnityEngine.Input.GetButton("Jump");
-
-        var x = UnityEngine.Input.GetAxis("Horizontal");
-        var y = UnityEngine.Input.GetAxis("Vertical");
+        input.Jump = _jumpInputAction.IsPressed();
+        Vector2 moveValue = _moveInputAction.ReadValue<Vector2>();
 
         // Input that is passed into the simulation needs to be deterministic that's why it's converted to FPVector2.
-        input.Direction = new Vector2(x, y).ToFPVector2();
+        input.Direction = new Vector2(moveValue.x, moveValue.y).ToFPVector2();
 
         callback.SetInput(input, DeterministicInputFlags.Repeatable);
     }
